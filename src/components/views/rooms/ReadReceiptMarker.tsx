7a702bb2b9c275d2f9175fc4f373c401f5832b85
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-01-26 23:55:56 +0330

    some ui changes

--- src/components/views/rooms/ReadReceiptMarker.tsx
+++ src/components/views/rooms/ReadReceiptMarker.tsx
@@ -176,7 +176,7 @@ export default class ReadReceiptMarker extends React.PureComponent<IProps, IStat
 
         const style = {
             left: toPx(this.props.leftOffset),
-            top: '15px',
+            top: '20px',
         };
 
         let title;
