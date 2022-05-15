e1397ac8f8c1c43bc0dba04b5710c172894f2b87
zandi2015 <zandi2015@gmail.com>
2022-05-15 20:06:17 +0430

    update background image for every hour

--- src/components/structures/RoomViewBackground.js
+++ src/components/structures/RoomViewBackground.js
@@ -3,7 +3,9 @@ import ThemeWatcher from "../../settings/watchers/ThemeWatcher";
 
 // className={mainClasses} ref={this.roomView} onKeyDown={this.onReactKeyDown}
 const RoomViewBackground = (props) => {
-    let rndNember= Math.floor(Math.random() * 1000) + 1;
+    // let rndNember= Math.floor(Math.random() * 1000) + 1;
+    var today = new Date();
+    var dateAndTime = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+'-'+today.getHours();
     let BackgroundImage = "https://static.fanoos.app/stellarium_night_compress.png";
     let theme = new ThemeWatcher().getEffectiveTheme();
     if (theme.includes("light")) {
@@ -13,7 +15,7 @@ const RoomViewBackground = (props) => {
         <div className={props.className}
             ref={props.ref}
             onKeyDown={props.onKeyDown}
-            style={{backgroundImage:`url(${BackgroundImage}?v=${rndNember})`}}>
+            style={{backgroundImage:`url(${BackgroundImage}?v=${dateAndTime})`}}>
             {props.children}
         </div>
     )
